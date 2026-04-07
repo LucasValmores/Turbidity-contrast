5fb3d8fefe7fb0247edf9754f84f7cc6d686e25d
Lucas Valmores <lucasfvalmores@proton.me>
2026-04-07 16:40:47 -0400

    Create turbidityCheck.py

---
+++ turbidityCheck.py
@@ -0,0 +1,28 @@
+import cv2
+
+#import images
+imgLeftTestTube = cv2.imread("C:/Users\Lucas/Pictures/left test tube gray scale/left test tube.jpeg", cv2.IMREAD_GRAYSCALE)
+imgRightTestTube = cv2.imread("C:/Users/Lucas/Pictures/left test tube gray scale/right test tube gray scale.jpeg", cv2.IMREAD_GRAYSCALE)
+imgBackground= cv2.imread("C:/Users/Lucas/Pictures/left test tube gray scale/background sample.jpeg", cv2.IMREAD_GRAYSCALE)
+
+#take average grayscale value of all three images
+leftmean = cv2.mean(imgLeftTestTube, mask=None)[0]
+rightMean = cv2.mean(imgRightTestTube, mask=None)[0]
+BackgroundMean = cv2.mean(imgBackground, mask=None)[0]
+
+
+print("average grayscale values of left test tube:", leftmean)
+print("average grayscale values of right test tube:", rightMean)
+print("average grayscale values of background:", BackgroundMean)
+
+LeftContrast = leftmean - BackgroundMean
+RightContrast = rightMean - BackgroundMean
+
+#display contrast
+print("left testTube contrast: ", LeftContrast)
+print("right testTube contrast: ", RightContrast)
+
+
+
+
+
